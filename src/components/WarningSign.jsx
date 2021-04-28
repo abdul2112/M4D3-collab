a175a2daacee432ea97232e99cfd3558b533ff13
Helena Nel <mailhedri@gmail.com>
2021-04-28 14:48:45 +0200

    Update WarningSign.jsx

---
+++ src/components/WarningSign.jsx
@@ -0,0 +1,26 @@
+import { Component } from "react";
+import { Alert, Container } from "react-bootstrap";
+
+class MyAlert extends Component {
+  render() {
+    return (
+      <Container>
+        <Alert variant="success">
+          <Alert.Heading>Hey, nice to see you</Alert.Heading>
+          <p>
+            Aww yeah, you successfully read this important alert message. This
+            example text is going to run a bit longer so that you can see how
+            spacing within an alert works with this kind of content.
+          </p>
+          <hr />
+          <p className="mb-0">
+            Whenever you need to, be sure to use margin utilities to keep things
+            nice and tidy.
+          </p>
+        </Alert>
+      </Container>
+    );
+  }
+}
+
+export default MyAlert;
